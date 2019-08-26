1dbefc5ac1c29f99da895cf5a5b0d56e183ca254
zjkl19 <eamdfan@126.com>
2019-08-26 17:59:48 +0800

    change db

--- PythonApplication/PythonApplication.py
+++ PythonApplication/PythonApplication.py
@@ -14,11 +14,25 @@ import matplotlib.ticker as ticker
 from brokenaxes import brokenaxes
 
 #定义函数来显示柱状上的数值
-def autolabel(rects):
+def autolabel(rects,y_data):
+    i=0
+    space=-0.20
     for rect in rects:
+        if 100>y_data[i]>=0:
+            space=-0.05
+        elif 1000>y_data[i]>=100:
+            space=-0.08
+        elif 10000>y_data[i]>=1000:
+            space=-0.10
+        elif 100000>y_data[i]>=10000:
+            space=-0.18
+        elif 1000000>y_data[i]>=100000:
+            space=-0.20
+        else:
+            space=-0.26
         height = rect.get_height()
-        plt.text(rect.get_x()+rect.get_width()/2.-0.28, 1.03*height, '%s' % height,fontsize=15)
-
+        plt.text(rect.get_x()+rect.get_width()/2.+space*5.0, 1.03*height, '%s' % height,fontsize=15)
+        i=i+1
 fontSize=15
 #plt.rcParams['figure.figsize'] = (8.0, 6.0)
 plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
@@ -26,19 +40,19 @@ plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 plt.xticks(fontsize=fontSize)
 plt.yticks(fontsize=fontSize)
 
+totalDays=7    #总天数
+
 # 包含每个柱子对应值的序列
-#values = [983272,15157,1433,140]
+y_data = [333943,5105,639,54]    #车重
+y_data = [77929,97206,89124,75482]    #车道
+y_data = [38635,204613,91268,5225]    #车速
+#y_data = [int(77929/totalDays),int(97206/totalDays),int(89124/totalDays),int(75482/totalDays)]    #不同车道日均车辆数
+y_data = [8915,4994,4784,26106,38978,37899,31443,38116,41328,39592,40428,27157]    #不同小时车辆总数
+y_data = [59.2,61.0,60.1,47.2,33.6,41.3,49.0,42.4,38.9,38.0,44.2,51.6]    #不同小时平均车速
 
+y_data = [59.2,61.0,60.1,47.2,33.6,41.3,49.0,42.4,38.9,38.0,44.2,51.6]    #不同小时平均车速
 
-y_data = [983272,15157,1433,140]    #车重
-#y_data = [17091,31998,91890,237471]    #车速
-y_data = [86973,108276,99618,83583]    #车道
-y_data = [49089,220678,102635,6048]    #车速
-y_data = [int(86973/14),int(108276/14),int(99618/14),int(83583/14)]    #不同车道日均车辆数
-y_data = [11579,6662,6296,30954,46563,43551,33158,35383,45446,49896,41109,27853]    #小时
-
-y_data = [11579,6662,6296,30954,46563,43551,33158,35383,45446,49896,41109,27853]    #小时
-x_data = range(1,13)
+x_data = range(1,len(y_data)+1)
 multiples = [x/sum(y_data) for x in y_data]
 
 xticksPrefix=['0～10t','10～20t','20～30t','30t以上']    #车重
@@ -46,7 +60,7 @@ xticksPrefix=['车道1','车道2','车道3','车道4']    #车道
 xticksPrefix=['0～30km/h','30～50km/h','50～70km/h','70km/h以上']    #车速
 xticksPrefix=['0～2','2～4','4～6','6～8','8～10','10～12','12～14','14～16','16～18','18～20','20～22','22～24']    #小时
 
-xticksPrefix=['0～2','2～4','4～6','6～8','8～10','10～12','12～14','14～16','16～18','18～20','20～22','22～24']
+xticksPrefix=['0～2','2～4','4～6','6～8','8～10','10～12','12～14','14～16','16～18','18～20','20～22','22～24']    #小时
 
 # 柱子总数
 N =len(y_data)
@@ -63,7 +77,7 @@ width = 0.5
 # 绘图
 p2=bax.bar(x_data,y_data, label='动态称重', color='steelblue',width=width,alpha=0.8)
 
-autolabel(p2)
+autolabel(p2,y_data)
 
 # 在柱状图上显示具体数值, ha参数控制水平对齐方式, va控制垂直对齐方式
 #for x, y in enumerate(y_data):
@@ -73,21 +87,30 @@ autolabel(p2)
 
 # 为两条坐标轴设置名称
 #plt.xlabel("年份")
-plt.ylabel("数量",fontsize=15)
+plt.ylabel("车速（km/h）",fontsize=15)
 
 # 添加纵横轴的刻度
-xticksLabel=['%s'%(xticksPrefix[0])+'\n(%.2f%%'%(100*multiples[0])+')']
-for i in range(len(xticksPrefix)):
-    plt.xticks(x_data, ['%s'%(xticksPrefix[0])+'\n(%.2f%%'%(100*multiples[0])+')', '%s'%(xticksPrefix[1])+'\n(%.2f%%'%(100*multiples[1])+')', '%s'%(xticksPrefix[2])+'\n(%.2f%%'%(100*multiples[2])+')', '%s'%(xticksPrefix[3])+'\n(%.2f%%'%(100*multiples[3])+')'])
+#xticksLabel=['%s'%(xticksPrefix[0])+'\n(%.2f%%'%(100*multiples[0])+')']
+#for i in range(len(xticksPrefix)):
+#    plt.xticks(x_data, ['%s'%(xticksPrefix[0])+'\n(%.2f%%'%(100*multiples[0])+')', '%s'%(xticksPrefix[1])+'\n(%.2f%%'%(100*multiples[1])+')', '%s'%(xticksPrefix[2])+'\n(%.2f%%'%(100*multiples[2])+')', '%s'%(xticksPrefix[3])+'\n(%.2f%%'%(100*multiples[3])+')'])
 
 # 添加纵横轴的刻度(不含比例)
-xticksLabel=['%s'%(xticksPrefix[0])]
-for i in range(len(xticksPrefix)):
-    plt.xticks(x_data, ['%s'%(xticksPrefix[0]), '%s'%(xticksPrefix[1]), '%s'%(xticksPrefix[2]), '%s'%(xticksPrefix[3])])
+#xticksLabel=['%s'%(xticksPrefix[0])]
+#for i in range(len(xticksPrefix)):
+#    plt.xticks(x_data, ['%s'%(xticksPrefix[0]), '%s'%(xticksPrefix[1]), '%s'%(xticksPrefix[2]), '%s'%(xticksPrefix[3])])
 
 xticksLabel=['%s'%(xticksPrefix[0])+'\n(%.2f%%'%(100*multiples[0])+')']
 for i in range(1,len(xticksPrefix)):
     xticksLabel=xticksLabel+['%s'%(xticksPrefix[i])+'\n(%.2f%%'%(100*multiples[i])+')']
+
+xticksLabel=['%s'%(xticksPrefix[0])]
+for i in range(1,len(xticksPrefix)):
+    xticksLabel=xticksLabel+['%s'%(xticksPrefix[i])]
+
+xticksLabel=['%s'%(xticksPrefix[0])]
+for i in range(1,len(xticksPrefix)):
+    xticksLabel=xticksLabel+['%s'%(xticksPrefix[i])]
+
 
 plt.xticks(x_data, xticksLabel)
 
@@ -101,6 +124,6 @@ ax.spines['right'].set_visible(False)
 
 # 显示图例
 #plt.legend()
-plt.savefig('tmp.pdf', bbox_inches='tight')
+#plt.savefig('tmp.pdf', bbox_inches='tight')
 plt.show()
 
