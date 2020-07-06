f0ac643d0e45a1e32213e43cd45af63b5a9892b0
迪南 林 <eamdfan@hotmail.com>
2020-07-06 21:43:17 +0800

    update for 202006

--- HelinConsoleApp/Program.cs
+++ HelinConsoleApp/Program.cs
@@ -81,8 +81,8 @@ namespace HelinConsoleApp
             var Gross_Load_Dist = new List<int>();
             var Gross_Load_Dist2 = new List<int>();
 
-            var StartDataTime = new DateTime(2020, 5, 1, 0, 0, 0);
-            var FinishDataTime = new DateTime(2020, 6, 1, 0, 0, 0);
+            var StartDataTime = new DateTime(2020, 6, 1, 0, 0, 0);
+            var FinishDataTime = new DateTime(2020, 7, 1, 0, 0, 0);
 
             //Expression<Func<HS_Data_201908, bool>> dataPredicate = x => x.HSData_DT >= StartDataTime && x.HSData_DT <= FinishDataTime;
 
@@ -92,7 +92,7 @@ namespace HelinConsoleApp
                 {
                     #region HS_DataForAnalysis
                     var HS_DataForAnalysis = (
-                        from c in db.HS_Data_202004
+                        from c in db.HS_Data_202005
                         select new MyHS_Data
                         {
                             Acceleration = c.Acceleration,
@@ -135,7 +135,7 @@ namespace HelinConsoleApp
                             Gross_Load = c.Gross_Load
                         }
                         ).Union(
-                        from e in db.HS_Data_202005
+                        from e in db.HS_Data_202006
                         select new MyHS_Data
                         {
                             Acceleration = e.Acceleration,
